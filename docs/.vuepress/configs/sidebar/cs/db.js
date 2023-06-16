7224f65da328290996c2f7e097b233ed051cf8fb
mosquito <mosquito@email.cn>
2023-06-16 16:49:25 +0800

    transaction

--- docs/.vuepress/configs/sidebar/cs/db.js
+++ docs/.vuepress/configs/sidebar/cs/db.js
@@ -2,7 +2,11 @@ export const db = {
   "/cs/db/": [
     {
       text: "数据厍理论",
-      children: ["/cs/db/01-数据库设计"],
+      children: [
+        "/cs/db/01-数据库设计",
+        "/cs/db/02-事务隔离性",
+        "/cs/db/03-完整性",
+      ],
     },
   ],
 };
